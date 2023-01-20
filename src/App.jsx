6a708854f6071f9b50d63c73230584fe9c1b6955
mohamud <mohmud141@gmail.com>
2023-01-20 16:11:12 +0300

    changes made in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,22 @@ const App = () => (
     <div className='bg-primary w-full overfollow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-        </div>
               <Navbar />
+        </div>
        </div>
+       
           <div className={`bg-primary ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
                   <Hero />
             </div>
-
           </div>
-          <div className={`bg-primary ${styles.flexStart}`}>
+
+          <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
                   <Stats /> 
                   <Business /> 
-                  <CardDeal /> 
                   <Billing /> 
+                  <CardDeal /> 
                   <Testimonials /> 
                   <Clients /> 
                   <CTA /> 

17c29137a3bd60f039a8a6738f799885eef6530d
mohamud <mohmud141@gmail.com>
2023-01-21 14:36:48 +0300

    business section added

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ const App = () => (
         <div className={`${styles.boxWidth}`}>
               <Navbar />
         </div>
-       </div>
-       
+       </div>   
           <div className={`bg-primary ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
                   <Hero />
--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import { features } from "../constants"
+import styles, {layout} from "../style"
+import Button from './Button'
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You Do the Business, <br className="sm:block hidden"/> We'll handle the money.</h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-[5]`}>With the right credit card, you can impove your financial life by building credit, earning rewards and saving money.
+            But with the hundreds of credit cards in the markey.
+          </p>
+            <Button  style="mt-10"/>
+      </div>
+    </section>
   )
 }
 

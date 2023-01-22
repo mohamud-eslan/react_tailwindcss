646728d127dd0c238e276e0ad405224703590a4d
mohamud <mohmud141@gmail.com>
2023-01-22 17:24:26 +0300

    div added

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, {layout} from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
+const CardDeal = () => (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br sm:block hidden/> in few steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}> Best credit card offers. Chase Freedom Unlimited®: Earn bonus cash back in 
+          popular categories, including dining and some</p>
+
+          <Button styles="mt-10"/>
+      </div>
+
+    </section>
   )
-}
+
 
 export default CardDeal
